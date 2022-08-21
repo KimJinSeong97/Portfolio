e0ef7324286445e30e99f344de05425a2f111026
KimJinSeong97 <100419154+KimJinSeong97@users.noreply.github.com>
2022-08-21 23:40:03 +0900

    220821
    intro design edit.

--- js/index.js
+++ js/index.js
@@ -1,10 +1,14 @@
-$("nav .menu").click(function () {
-    $('nav .circle a').css("background", "rgba(50,50,50,.5)");
-    $('nav .circle').stop().toggleClass('hidenav');
-});
-$("a").click(function () {
-    $(this).css("background", "rgba(255,200,0,1)");
-    setTimeout(function () {
-        $('nav .circle').stop().addClass('hidenav');
-    }, 1000);
-});
+$(function(){
+    $('.menu').click(function(){
+        $('.sub_menu').fadeToggle(500);
+    })
+})
+
+$(window).scroll(function(){
+//     var scrolling = $(this).scrollTop();
+// 
+//     var scroll_intro = $('#intro').offset().top;
+//     if(scrolling > scroll_intro - 100){
+//         $('#intro h2, #intro h3').css('opacity', '0');
+//     }
+})

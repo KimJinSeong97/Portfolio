dc5d3de2f93232228258773166c27d8cff47db62
KimJinSeong97 <100419154+KimJinSeong97@users.noreply.github.com>
2022-08-25 17:34:36 +0900

    220825
    #project font effect, design edit.

--- js/index.js
+++ js/index.js
@@ -1,10 +1,5 @@
-// $(function () {
-//     $('body').css('display', 'none');
-//     $('body').fadeIn(2000, redirectPage);
-//     function redirectPage() {
-//         window.location = linkLocation;
-//     }
-// })
+//새로고침하면 무조건 제일 위로
+history.scrollRestoration = "manual"
 
 //페이지 접속시 인트로 애니메이션
 var intro = gsap.timeline({
@@ -19,3 +14,5 @@ intro.to('#intro h2', 0.75, { color: 'black', ease: 'power0.out' }, 2.5);
 intro.fromTo('.bottom_box', 1.5, { y: 0 + 'vw' }, { y: 0 + 'vw', height: 1000 + 'px', ease: 'expo.out' }, 2.5);
 intro.fromTo('#intro h2', 1, { y: 0, opacity: 1 }, { y: -50, opacity: 0 }, 3.5);
 intro.fromTo('.bottom_box2', 1.5, { y: 0 + 'vw' }, { y: 0 + 'vw', height: 1000 + 'px', ease: 'expo.out' }, 4.5);
+intro.fromTo('header', 1, { y: 25, opacity: 0 }, { y: 0, opacity: 1 }, 6);
+intro.fromTo('.introduce h3', 1, { y: 25, opacity: 0 }, { y: 0, opacity: 1 }, 6.5);

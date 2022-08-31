61ceddac94cb5c9404befd765487f6169ef2f519
KimJinSeong97 <100419154+KimJinSeong97@users.noreply.github.com>
2022-08-31 18:24:22 +0900

    220831
    Intro Text, img design edit.
    Skill Icon text, img design edit.

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
-//새로고침하면 무조건 제일 위로
-history.scrollRestoration = "manual"
+// //새로고침하면 무조건 제일 위로
+// history.scrollRestoration = "manual"
 
 //페이지 접속시 인트로 애니메이션
 var intro = gsap.timeline({
@@ -15,13 +15,17 @@ intro.fromTo('.bottom_box', 1.5, { y: 0 + 'vw' }, { y: 0 + 'vw', height: 1000 + 
 intro.fromTo('#intro h2', 1, { y: 0, opacity: 1 }, { y: -50, opacity: 0 }, 3.5);
 intro.fromTo('.bottom_box2', 1.5, { y: 0 + 'vw' }, { y: 0 + 'vw', height: 1000 + 'px', ease: 'expo.out' }, 4.5);
 intro.fromTo('header', 1, { y: 25, opacity: 0 }, { y: 0, opacity: 1 }, 6);
-intro.fromTo('.introduce h3', 1, { y: 25, opacity: 0 }, { y: 0, opacity: 1 }, 6.5);
+intro.fromTo('.introduce img', 1, { y: 25, opacity: 0 }, { y: 0, opacity: 1 }, 7);
+intro.fromTo('.introduce>p', 1, { y: 25, opacity: 0 }, { y: 0, opacity: 1 }, 7.5);
+intro.fromTo('.strength .positive', 1, { y: 25, opacity: 0 }, { y: 0, opacity: 1 }, 8);
+intro.fromTo('.strength .responsible', 1, { y: 25, opacity: 0 }, { y: 0, opacity: 1 }, 8.5);
+intro.fromTo('.strength .communicative', 1, { y: 25, opacity: 0 }, { y: 0, opacity: 1 }, 9);
 
-//스크롤하면 나타나는 프로젝트
+//스크롤내리면 프로젝트가 나타남
 $(window).scroll(function () {
     var scrollTop = $(this).scrollTop();
-
     var scroll_project = $('#project .inner').offset().top;
+
     if (scrollTop > scroll_project + 0) {
         $('.shure').css('transform', 'translate(0px)').css({ opacity: 1 });
     }
@@ -37,4 +41,36 @@ $(window).scroll(function () {
     if (scrollTop > scroll_project + 4000) {
         $('.applemusic').css('transform', 'translate(0px)').css({ opacity: 1 });
     }
+})
+
+//Skill 마우스오버하면 이미지, 내용 바뀜
+$(function () {
+    $('.html').mouseover(function () {
+        $('.skill_icon img').attr('src', 'img/html.png')
+        $('.skill_icon p').html('<p>웹 표준과 웹 접근성을 지킵니다.</p>')
+        $(this).addClass('bold_font')
+        $('.css, .js, .photoshop').removeClass('bold_font')
+        $('.skill_icon').css('transform', 'translate(0px)').css({ opacity: 1 })
+    });
+    $('.css').mouseover(function () {
+        $('.skill_icon img').attr('src', 'img/sass.png')
+        $('.skill_icon p').html('<p>미디어 퀴리로 반응형 웹페이지를<br>제작할 수 있습니다.<br>SCSS 문법을 사용합니다.<br>주로 Nesting, Mixin을 사용합니다.</p>')
+        $(this).addClass('bold_font')
+        $('.html, .js, .photoshop').removeClass('bold_font')
+        $('.skill_icon').css('transform', 'translate(0px)').css({ opacity: 1 })
+    });
+    $('.js').mouseover(function () {
+        $('.skill_icon img').attr('src', 'img/js.png')
+        $('.skill_icon p').html('<p>JS와 jQuery를 이용해 캐러셀을 제작할 수 있습니다.<br>jQuery로 상호작용(네비게이션, 탭 메뉴,<br>모달창, 아코디언)을 제작할 수 있습니다.</p>')
+        $(this).addClass('bold_font')
+        $('.html, .css, .photoshop').removeClass('bold_font')
+        $('.skill_icon').css('transform', 'translate(0px)').css({ opacity: 1 })
+    });
+    $('.photoshop').mouseover(function () {
+        $('.skill_icon img').attr('src', 'img/photoshop.png')
+        $('.skill_icon p').html('<p>포토샵으로 이미지를 자유롭게 편집할 수 있습니다.</p>')
+        $(this).addClass('bold_font')
+        $('.css, .js, .html').removeClass('bold_font')
+        $('.skill_icon').css('transform', 'translate(0px)').css({ opacity: 1 })
+    });
 })

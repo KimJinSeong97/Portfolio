97e5d6f84f26c2e6e84558ec8b5db0de17fce7c5
KimJinSeong97 <100419154+KimJinSeong97@users.noreply.github.com>
2022-09-01 18:53:53 +0900

    220901
    intro design font edit. #about me plus.

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
-// //새로고침하면 무조건 제일 위로
-// history.scrollRestoration = "manual"
+//새로고침하면 무조건 제일 위로
+history.scrollRestoration = "manual"
 
 //페이지 접속시 인트로 애니메이션
 var intro = gsap.timeline({
@@ -7,40 +7,27 @@ var intro = gsap.timeline({
 });
 
 intro.fromTo('#intro h2', 1, { y: 50, opacity: 0 }, { y: 0, opacity: 1 }, 0);
-intro.fromTo('.left_box', 1.25, { x: -200 + 'vw' }, { x: 0 + 'vw', width: 49.7 + 'vw', ease: 'power3.out' }, 1);
-intro.fromTo('.right_box', 1.25, { x: 200 + 'vw' }, { x: 0 + 'vw', width: 49.7 + 'vw', ease: 'power3.out' }, 1);
-intro.fromTo('#intro h2', 2, { color: 'black' }, { color: 'white', ease: 'power0.out' }, 1);
-intro.to('#intro h2', 0.75, { color: 'black', ease: 'power0.out' }, 2.5);
-intro.fromTo('.bottom_box', 1.5, { y: 0 + 'vw' }, { y: 0 + 'vw', height: 1000 + 'px', ease: 'expo.out' }, 2.5);
-intro.fromTo('#intro h2', 1, { y: 0, opacity: 1 }, { y: -50, opacity: 0 }, 3.5);
-intro.fromTo('.bottom_box2', 1.5, { y: 0 + 'vw' }, { y: 0 + 'vw', height: 1000 + 'px', ease: 'expo.out' }, 4.5);
-intro.fromTo('header', 1, { y: 25, opacity: 0 }, { y: 0, opacity: 1 }, 6);
-intro.fromTo('.introduce img', 1, { y: 25, opacity: 0 }, { y: 0, opacity: 1 }, 7);
-intro.fromTo('.introduce>p', 1, { y: 25, opacity: 0 }, { y: 0, opacity: 1 }, 7.5);
-intro.fromTo('.strength .positive', 1, { y: 25, opacity: 0 }, { y: 0, opacity: 1 }, 8);
-intro.fromTo('.strength .responsible', 1, { y: 25, opacity: 0 }, { y: 0, opacity: 1 }, 8.5);
-intro.fromTo('.strength .communicative', 1, { y: 25, opacity: 0 }, { y: 0, opacity: 1 }, 9);
+intro.fromTo('.left_box', 1.25, { x: -200 + 'vw' }, { x: 0 + 'vw', width: 49.7 + 'vw', ease: 'power3.out' }, 0.5);
+intro.fromTo('.right_box', 1.25, { x: 200 + 'vw' }, { x: 0 + 'vw', width: 49.7 + 'vw', ease: 'power3.out' }, 0.5);
+intro.fromTo('#intro h2', 2, { color: 'black' }, { color: 'white', ease: 'power0.out' }, 0.5);
+intro.to('#intro h2', 1, { color: 'black', ease: 'power0.out' }, 1.75);
+intro.fromTo('.bottom_box', 1.25, { y: 0 + 'vh' }, { y: 0 + 'vh', height: 100 + 'vh', ease: 'expo.out' }, 1.75);
+intro.fromTo('#intro h2', 1, { y: 0, opacity: 1 }, { y: -50, opacity: 0 }, 2.5);
+intro.fromTo('.bottom_box2', 1.25, { y: 0 + 'vh' }, { y: 0 + 'vh', height: 100 + 'vh', ease: 'expo.out' }, 3.5);
+intro.fromTo('header', 0.5, { y: 25, opacity: 0 }, { y: 0, opacity: 1 }, 4);
+intro.fromTo('.introduce h3', 0.5, { y: 20, opacity: 0 }, { y: 0, opacity: 1 }, 4.5);
+intro.fromTo('.introduce img', 0.5, { y: 20, opacity: 0 }, { y: 0, opacity: 1 }, 5);
+intro.fromTo('.introduce h4', 0.5, { y: 20, opacity: 0 }, { y: 0, opacity: 1 }, 5.5);
+intro.fromTo('.introduce>p', 0.5, { y: 20, opacity: 0 }, { y: 0, opacity: 1 }, 6);
 
-//스크롤내리면 프로젝트가 나타남
-$(window).scroll(function () {
-    var scrollTop = $(this).scrollTop();
-    var scroll_project = $('#project .inner').offset().top;
+//인트로 스크롤시 글씨 좌,우로 움직임
+window.addEventListener('scroll', function () {
+    let scroll_value = window.scrollY;
+    let intro_comment1 = document.querySelector('.introduce h3');
+    let intro_comment2 = document.querySelector('.introduce h4');
 
-    if (scrollTop > scroll_project + 0) {
-        $('.shure').css('transform', 'translate(0px)').css({ opacity: 1 });
-    }
-    if (scrollTop > scroll_project + 1000) {
-        $('.talesweaver').css('transform', 'translate(0px)').css({ opacity: 1 });
-    }
-    if (scrollTop > scroll_project + 2000) {
-        $('.millie').css('transform', 'translate(0px)').css({ opacity: 1 });
-    }
-    if (scrollTop > scroll_project + 3000) {
-        $('.baskinrobbins').css('transform', 'translate(0px)').css({ opacity: 1 });
-    }
-    if (scrollTop > scroll_project + 4000) {
-        $('.applemusic').css('transform', 'translate(0px)').css({ opacity: 1 });
-    }
+    intro_comment1.style.left = (scroll_value * 0.075) - 15 + '%';
+    intro_comment2.style.right = (scroll_value * 0.075) - 15 + '%';
 })
 
 //Skill 마우스오버하면 이미지, 내용 바뀜
@@ -73,4 +60,29 @@ $(function () {
         $('.css, .js, .html').removeClass('bold_font')
         $('.skill_icon').css('transform', 'translate(0px)').css({ opacity: 1 })
     });
+})
+
+//스크롤내리면 프로젝트가 나타남
+$(window).scroll(function () {
+    var scrollTop = $(this).scrollTop();
+    var scroll_project = $('#project .inner').offset().top;
+
+    //인트로 글씨 언더라인 생성 
+    $('.border_box1, .border_box2').css('width', 1300);
+
+    if (scrollTop > scroll_project + 0) {
+        $('.shure').css('transform', 'translate(0px)').css({ opacity: 1 });
+    }
+    if (scrollTop > scroll_project + 1000) {
+        $('.talesweaver').css('transform', 'translate(0px)').css({ opacity: 1 });
+    }
+    if (scrollTop > scroll_project + 2000) {
+        $('.millie').css('transform', 'translate(0px)').css({ opacity: 1 });
+    }
+    if (scrollTop > scroll_project + 3000) {
+        $('.baskinrobbins').css('transform', 'translate(0px)').css({ opacity: 1 });
+    }
+    if (scrollTop > scroll_project + 4000) {
+        $('.applemusic').css('transform', 'translate(0px)').css({ opacity: 1 });
+    }
 })

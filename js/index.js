b4c0ebad4d7843d9d7ce9449dedb6eb464d8bc0e
KimJinSeong97 <100419154+KimJinSeong97@users.noreply.github.com>
2022-09-14 09:57:21 +0900

    220911~220913
    I forgot commit when 0911~ 0913
    and i looking for contact design.

--- js/index.js
+++ js/index.js
@@ -22,17 +22,17 @@ intro.fromTo('.introduce h4', 0.5, { y: 20, opacity: 0 }, { y: 0, opacity: 1 }, 
 gsap.registerPlugin(ScrollTrigger);
 gsap.to(".img_animation1", 1, { scrollTrigger: "img_animation1", width: 100 + '%', ease: 'expo.inOut' }, 0.5);
 gsap.to(".img_animation2", 1.5, { scrollTrigger: "img_animation2", width: 100 + '%', ease: 'expo.inOut' }, 1);
-gsap.fromTo(".my_img1", 2.5, { scrollTrigger: ".my_img1", x: -750 }, { scrollTrigger: ".my_img1", x: 0, ease: 'expo.inOut' }, 2);
+gsap.fromTo(".my_img", 2.5, { scrollTrigger: ".my_img", x: -751 }, { scrollTrigger: ".my_img", x: 0, ease: 'expo.inOut' }, 2);
 gsap.fromTo(".introduce_btm h5", 1.5, { scrollTrigger: ".introduce_btm h5", y: 25, opacity: 0 }, { scrollTrigger: ".introduce_btm h5", y: 0, opacity: 1 }, 3);
 gsap.fromTo(".introduce_btm p", 1.5, { scrollTrigger: ".introduce_btm p", y: 50, opacity: 0 }, { scrollTrigger: ".introduce_btm p", y: 0, opacity: 1 }, 4);
-gsap.to('.flip:nth-of-type(1)', 0.5, { scrollTrigger: ".flip:nth-of-type(2)", y: 0, opacity: 1, ease: "expo.inOut" })
-gsap.to('.flip:nth-of-type(2)', 0.5, { scrollTrigger: ".flip:nth-of-type(3)", y: 0, opacity: 1, ease: "expo.inOut" })
-gsap.to('.flip:nth-of-type(3)', 0.5, { scrollTrigger: ".flip:nth-of-type(4)", y: 0, opacity: 1, ease: "expo.inOut" })
-gsap.to('.flip:nth-of-type(4)', 0.5, { scrollTrigger: ".flip:nth-of-type(5)", y: 0, opacity: 1, ease: "expo.inOut" })
-gsap.to('.flip:nth-of-type(5)', 0.5, { scrollTrigger: ".flip:nth-of-type(6)", y: 0, opacity: 1, ease: "expo.inOut" })
-gsap.to('.flip:nth-of-type(6)', 0.5, { scrollTrigger: ".flip:nth-of-type(7)", y: 0, opacity: 1, ease: "expo.inOut" })
-gsap.to('.flip:nth-of-type(7)', 0.5, { scrollTrigger: ".flip:nth-of-type(8)", y: 0, opacity: 1, ease: "expo.inOut" })
-gsap.to('.flip:nth-of-type(8)', 0.5, { scrollTrigger: ".flip:nth-of-type(8)", y: 0, opacity: 1, ease: "expo.inOut" })
+// gsap.to('.flip:nth-of-type(1)', 0.5, { scrollTrigger: ".flip:nth-of-type(2)", y: 0, opacity: 1, ease: "expo.inOut" })
+// gsap.to('.flip:nth-of-type(2)', 0.5, { scrollTrigger: ".flip:nth-of-type(3)", y: 0, opacity: 1, ease: "expo.inOut" })
+// gsap.to('.flip:nth-of-type(3)', 0.5, { scrollTrigger: ".flip:nth-of-type(4)", y: 0, opacity: 1, ease: "expo.inOut" })
+// gsap.to('.flip:nth-of-type(4)', 0.5, { scrollTrigger: ".flip:nth-of-type(5)", y: 0, opacity: 1, ease: "expo.inOut" })
+// gsap.to('.flip:nth-of-type(5)', 0.5, { scrollTrigger: ".flip:nth-of-type(6)", y: 0, opacity: 1, ease: "expo.inOut" })
+// gsap.to('.flip:nth-of-type(6)', 0.5, { scrollTrigger: ".flip:nth-of-type(7)", y: 0, opacity: 1, ease: "expo.inOut" })
+// gsap.to('.flip:nth-of-type(7)', 0.5, { scrollTrigger: ".flip:nth-of-type(8)", y: 0, opacity: 1, ease: "expo.inOut" })
+// gsap.to('.flip:nth-of-type(8)', 0.5, { scrollTrigger: ".flip:nth-of-type(8)", y: 0, opacity: 1, ease: "expo.inOut" })
 
 //인트로 스크롤시 글씨 좌,우로 움직임
 window.addEventListener('scroll', function () {
@@ -49,9 +49,9 @@ window.addEventListener('scroll', function () {
     let project = document.querySelector('#project h2');
     let contact = document.querySelector('#contact h2');
 
-    aboutme.style.marginLeft = (scroll_value * 0.9) + 'px';
-    project.style.marginLeft = ((scroll_value - 2000) * 0.9) + 'px';
-    contact.style.marginLeft = ((scroll_value - 7000) * 0.70) + 'px';
+    aboutme.style.marginLeft = (scroll_value * 0.85) + 'px';
+    project.style.marginLeft = ((scroll_value - 2000) * 0.85) + 'px';
+    contact.style.marginLeft = ((scroll_value - 7000) * 0.65) + 'px';
 })
 
 //aboutme my strength 폰트 뒤집기
@@ -107,19 +107,19 @@ $(window).scroll(function () {
 $(function () {
     //슈어 썸네일 호버, 클릭 시 소스 코드 보기.
     $('.shure_thumbnail').mouseover(function () {
-        $('.shure>img').css('filter', 'blur(3px)');
+        $('.shure>img').css('filter', 'brightness(50%)');
         $('.shure .languege').css('display', 'block');
     })
     $('.shure .languege .html_btn').click(function () {
-        $('.shure .html_code').css('display', 'block');
+        $('.shure .html_code').toggle();
         $('.shure .css_code, .shure .js_code').css('display', 'none');
     })
     $('.shure .languege .css_btn').click(function () {
-        $('.shure .css_code').css('display', 'block');
+        $('.shure .css_code').toggle();
         $('.shure .html_code, .shure .js_code').css('display', 'none');
     })
     $('.shure .languege .js_btn').click(function () {
-        $('.shure .js_code').css('display', 'block');
+        $('.shure .js_code').toggle();
         $('.shure .html_code, .shure .css_code').css('display', 'none');
     })
     $('.shure_thumbnail').mouseout(function () {
@@ -129,19 +129,73 @@ $(function () {
 
     //테일즈위버 썸네일 호버, 클릭 시 소스 코드 보기.
     $('.talesweaver_thumbnail').mouseover(function () {
-        $('.talesweaver>img').css('filter', 'blur(3px)');
+        $('.talesweaver>img').css('filter', 'brightness(50%)');
         $('.talesweaver .languege').css('display', 'block');
     })
     $('.talesweaver .languege .html_btn').click(function () {
-        $('.talesweaver .html_code').css('display', 'block');
+        $('.talesweaver .html_code').toggle();
         $('.talesweaver .css_code').css('display', 'none');
     })
     $('.talesweaver .languege .css_btn').click(function () {
+        $('.talesweaver .css_code').toggle();
         $('.talesweaver .html_code').css('display', 'none');
-        $('.talesweaver .css_code').css('display', 'block');
     })
     $('.talesweaver_thumbnail').mouseout(function () {
         $('.talesweaver>img').css('filter', 'none')
         $('.talesweaver .languege').css('display', 'none');
+    })
+
+    //밀리의서재 썸네일 호버, 클릭 시 소스 코드 보기.
+    $('.millie_thumbnail').mouseover(function () {
+        $('.millie>img').css('filter', 'brightness(50%)');
+        $('.millie .languege').css('display', 'block');
+    })
+    $('.millie .languege .html_btn').click(function () {
+        $('.millie .html_code').toggle();
+        $('.millie .js_code').css('display', 'none');
+    })
+    $('.millie .languege .js_btn').click(function () {
+        $('.millie .js_code').toggle();
+        $('.millie .html_code').css('display', 'none');
+    })
+    $('.millie_thumbnail').mouseout(function () {
+        $('.millie>img').css('filter', 'none')
+        $('.millie .languege').css('display', 'none');
+    })
+
+    //배스킨라빈스 썸네일 호버, 클릭 시 소스 코드 보기.
+    $('.baskinrobbins_thumbnail').mouseover(function () {
+        $('.baskinrobbins>img').css('filter', 'brightness(50%)');
+        $('.baskinrobbins .languege').css('display', 'block');
+    })
+    $('.baskinrobbins .languege .html_btn').click(function () {
+        $('.baskinrobbins .html_code').toggle();
+        $('.baskinrobbins .js_code').css('display', 'none');
+    })
+    $('.baskinrobbins .languege .js_btn').click(function () {
+        $('.baskinrobbins .js_code').toggle();
+        $('.baskinrobbins .html_code').css('display', 'none');
+    })
+    $('.baskinrobbins_thumbnail').mouseout(function () {
+        $('.baskinrobbins>img').css('filter', 'none')
+        $('.baskinrobbins .languege').css('display', 'none');
+    })
+
+    //애플뮤직 썸네일 호버, 클릭 시 소스 코드 보기.
+    $('.applemusic_thumbnail').mouseover(function () {
+        $('.applemusic>img').css('filter', 'brightness(50%)');
+        $('.applemusic .languege').css('display', 'block');
+    })
+    $('.applemusic .languege .html_btn').click(function () {
+        $('.applemusic .html_code').toggle();
+        $('.applemusic .css_code').css('display', 'none');
+    })
+    $('.applemusic .languege .css_btn').click(function () {
+        $('.applemusic .css_code').toggle();
+        $('.applemusic .html_code').css('display', 'none');
+    })
+    $('.applemusic_thumbnail').mouseout(function () {
+        $('.applemusic>img').css('filter', 'none')
+        $('.applemusic .languege').css('display', 'none');
     })
 })
